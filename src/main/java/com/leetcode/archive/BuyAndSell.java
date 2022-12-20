383a61e4aa9e8ff692731ba21fa06ec782241e79
Erol Hadzhi <erol.hadzhi@company.com>
2022-12-20 16:24:39 +0200

    buy and sell

---
+++ src/main/java/com/leetcode/archive/BuyAndSell.java
@@ -0,0 +1,13 @@
+package com.leetcode.archive;
+
+public class BuyAndSell {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
